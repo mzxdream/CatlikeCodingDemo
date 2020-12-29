fd19d36e8bb892f7e42895de6125b4e95d85ae11
mzxdream <mzxdream@gmail.com>
2020-12-29 20:07:37 +0800

    1

--- TowerDefense/TheBoard/Assets/Scripts/GameBoard.cs
+++ TowerDefense/TheBoard/Assets/Scripts/GameBoard.cs
@@ -1,18 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GameBoard : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    Transform ground = default;
+    Vector2Int size;
+    public void Initialize(Vector2Int size)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        this.size = size;
+        ground.localScale = new Vector3(size.x, size.y, 1f);
     }
 }
