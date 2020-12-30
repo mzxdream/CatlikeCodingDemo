14e5df12b86ff6142f27ffad9f60d2205e9bddcc
mengzhaoxiang <mengzhaoxiang@xd.com>
2020-12-30 11:04:25 +0800

    1

--- TowerDefense/TheBoard/Assets/Scripts/GameBoard.cs
+++ TowerDefense/TheBoard/Assets/Scripts/GameBoard.cs
@@ -5,9 +5,22 @@ public class GameBoard : MonoBehaviour
     [SerializeField]
     Transform ground = default;
     Vector2Int size;
+    [SerializeField]
+    GameTile tilePrefab = default;
     public void Initialize(Vector2Int size)
     {
         this.size = size;
         ground.localScale = new Vector3(size.x, size.y, 1f);
+
+        Vector2 offset = new Vector2((size.x - 1) * 0.5f, (size.y - 1) * 0.5f);
+        for (int y = 0; y < size.y; y++)
+        {
+            for (int x = 0; x < size.x; x++)
+            {
+                GameTile tile = Instantiate(tilePrefab);
+                tile.transform.SetParent(transform, false);
+                tile.transform.localPosition = new Vector3(x - offset.x, 0, y - offset.y);
+            }
+        }
     }
 }
